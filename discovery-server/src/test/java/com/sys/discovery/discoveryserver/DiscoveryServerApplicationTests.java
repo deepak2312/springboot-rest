0e4997f5576ce664971c1d710c85531be1145598
deepak <deepak@ubantu>
2021-08-24 23:58:49 +0530

    Initial Commit

---
+++ discovery-server/src/test/java/com/sys/discovery/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sys.discovery.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
